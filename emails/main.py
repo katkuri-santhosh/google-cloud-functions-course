32abb379cdb15c38a07ca1dca493d03ccd4ad038
katkuri-santhosh <86968250+katkuri-santhosh@users.noreply.github.com>
2021-07-15 13:21:10 +0200

    Update main.py

--- emails/main.py
+++ emails/main.py
@@ -55,4 +55,4 @@ def send_mail(request):
     except Exception as e:
         return e, 400
 
-send_mail(request)
+send_mail({"sender": "santusub4u@gmail.com","receiver": "katkuri.santhosh@gmail.com","subject": "Testing for new cloud function to send email","message": "Hi, if you are seeing this ,thats because the cloud function was invoked"})
