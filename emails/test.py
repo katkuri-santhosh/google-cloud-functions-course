b8f01cce0970a2d43d3fe233507452d280b238da
katkuri-santhosh <86968250+katkuri-santhosh@users.noreply.github.com>
2021-07-15 14:12:29 +0200

    Update test.py

--- emails/test.py
+++ emails/test.py
@@ -1,3 +1,3 @@
-from emails/main import send_mail
+from main.py import send_mail
 
 send_mail(sender= "santusub4u@gmail.com",receiver= "katkuri.santhosh@gmail.com",subject= "Testing for new cloud function to send email",message= "Hi, if you are seeing this ,thats because the cloud function was invoked")
